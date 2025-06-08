1b9cfd9d027153f3f12e7431ea3a48ccb98c1dd1
poneciak57 <poneciak@gmail.com>
2025-06-08 22:35:02 +0200

    updated clear.sql

--- db/clear.sql
+++ db/clear.sql
@@ -14,10 +14,20 @@ DROP FUNCTION IF EXISTS get_product_discount_info(INT, VARCHAR) CASCADE;
 DROP FUNCTION IF EXISTS get_min_price_30_days(INT) CASCADE;
 DROP FUNCTION IF EXISTS get_product_materials_info(INT) CASCADE;
 DROP FUNCTION IF EXISTS get_product_tags_info(INT) CASCADE;
+DROP FUNCTION IF EXISTS add_storage_delivery_part(INT, INT) CASCADE;
+DROP FUNCTION IF EXISTS calculate_order_price_at_timestamp(INT, TIMESTAMPTZ) CASCADE;
+DROP FUNCTION IF EXISTS get_order_total_at_timestamp(INT, TIMESTAMPTZ) CASCADE;
 
 -- Drop trigger functions
 DROP FUNCTION IF EXISTS trg_product_material_check_sum() CASCADE;
 DROP FUNCTION IF EXISTS trg_variant_size_sizing_type_coherence() CASCADE;
+DROP FUNCTION IF EXISTS order_status_view_insert() CASCADE;
+DROP FUNCTION IF EXISTS order_status_view_update() CASCADE;
+
+-- Drop views and rules
+DROP VIEW IF EXISTS product_details_view CASCADE;
+DROP VIEW IF EXISTS product_price_view CASCADE;
+DROP VIEW IF EXISTS order_status_view CASCADE;
 
 -- Drop all tables in reverse dependency order
 DROP TABLE IF EXISTS cart_product_variant CASCADE;
@@ -25,6 +35,7 @@ DROP TABLE IF EXISTS order_product CASCADE;
 DROP TABLE IF EXISTS order_history CASCADE;
 DROP TABLE IF EXISTS "order" CASCADE;
 DROP TABLE IF EXISTS product_material CASCADE;
+DROP TABLE IF EXISTS storage_quantity CASCADE;
 DROP TABLE IF EXISTS storage_delivery_part CASCADE;
 DROP TABLE IF EXISTS storage_delivery CASCADE;
 DROP TABLE IF EXISTS tag_product CASCADE;
